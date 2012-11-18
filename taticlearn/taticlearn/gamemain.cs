51c18ac86439d364f5211a78ddba33d99b3c983b
mdxbhmt <originalmandex@gmail.com>
2012-11-17 19:31:16 -0800

    ff

---
+++ taticlearn/taticlearn/gamemain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace taticlearn
+{
+    
+    class gamemain
+    {
+        private gameobject[,] grid;
+
+
+        public gamemain() { 
+        grid = new gameobject[10,10];
+        }
+
+    }
+}
